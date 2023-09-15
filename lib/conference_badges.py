7f3197c2fa5b7c2cdcc1a898a524bcafb65bc949
Walkerngeno <ngenobrayo7@gmail.com>
2023-09-15 14:59:29 +0300

    update changes

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,31 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []  # Create an empty list to store the badges
+    for name in names:
+        badge = badge_maker(name)  # Create a badge for each name
+        badges.append(badge)  # Add the badge to the list
+    return badges  # Return the list of badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []  # Create an empty list to store room assignments
+    for index, name in enumerate(names, 1):
+        room_assignment = f"Hello, {name}! You'll be in room {index}!"  # Add exclamation mark
+        room_assignments.append(room_assignment)  # Add the assignment to the list
+    return room_assignments  # Return the list of room assignments
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)  # Generate badges
+    room_assignments = assign_rooms(names)  # Generate room assignments
+    
+    for badge in badges:
+        print(badge)  # Print each badge
+    
+    for room_assignment in room_assignments:
+        print(room_assignment)  # Print each room assignment
+
+# Example usage:
+names = ['Guido', 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace']
+printer(names)
--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+# conftest.py
 
+import pytest
+
+@pytest.hookimpl(tryfirst=True)
 def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+    # Modify the item._nodeid attribute as needed
+    pref = "prefix"
+    suf = "suffix"
+    item._nodeid = ' '.join((pref, suf))
